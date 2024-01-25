2131f52c53c6b71a7df3b17be926a53fbc4e9d36
CoderBin <1669940775@qq.com>
2024-01-25 22:53:23 +0800

    feat: mdx 编译：编译测试

---
+++ src/node/__test__/md.test.ts
@@ -0,0 +1,29 @@
+import { unified } from 'unified';
+import { describe, test, expect } from 'vitest';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+
+describe('Markdown compile cases', () => {
+  // 初始化 processor，注册插件
+  const processor = unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeStringify);
+
+  // 测试标题
+  test('Compile title', async () => {
+    const mdContent = '# CoderBin';
+    const result = processor.processSync(mdContent);
+    expect(result.value).toMatchInlineSnapshot('"<h1>CoderBin</h1>"');
+  });
+
+  // 测试代码块
+  test('Compile code', async () => {
+    const mdContent = 'I am using `EasyPress.js`';
+    const result = processor.processSync(mdContent);
+    expect(result.value).toMatchInlineSnapshot(
+      '"<p>I am using <code>EasyPress.js</code></p>"'
+    );
+  });
+});
