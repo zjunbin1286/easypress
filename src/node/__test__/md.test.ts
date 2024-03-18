79adf0ffedc3dded8cf4eaa406efd9c1e1774179
CoderBin <1669940775@qq.com>
2024-03-18 22:26:25 +0800

    feat: 实现 preWrapper 插件

--- src/node/__test__/md.test.ts
+++ src/node/__test__/md.test.ts
@@ -3,13 +3,15 @@ import { describe, test, expect } from 'vitest';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
+import { rehypePluginPreWrapper } from '../../node/plugin-mdx/rehypePlugins/preWrapper';
 
 describe('Markdown compile cases', () => {
   // 初始化 processor，注册插件
   const processor = unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypeStringify);
+    .use(rehypeStringify)
+    .use(rehypePluginPreWrapper);
 
   // 测试标题
   test('Compile title', async () => {
@@ -25,5 +27,14 @@ describe('Markdown compile cases', () => {
     expect(result.value).toMatchInlineSnapshot(
       '"<p>I am using <code>EasyPress.js</code></p>"'
     );
+  });
+
+  test('Compile code block', async () => {
+    const mdContent = '```js\nconsole.log(123);\n```';
+    const result = processor.processSync(mdContent);
+    expect(result.value).toMatchInlineSnapshot(`
+      "<div class=\\"language-js\\"><span class=\\"lang\\">js</span><pre><code class=\\"\\">console.log(123);
+      </code></pre></div>"
+    `);
   });
 });
